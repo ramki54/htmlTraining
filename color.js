65925dc9336aab137be2f5fcd095be3504dc7323
Siva Kumar M <sivakumarm@Sivas-iMac.local>
2021-06-16 07:52:54 +0530

    Color changes for the table

---
+++ color.js
@@ -0,0 +1,11 @@
+function changeColorToRed() {
+    document.getElementById("redColor").style.color = "red";
+}
+
+function changeColorToGreen() {
+    document.getElementById("greenColor").style.color = "green";
+}
+
+function changeColorToBlue() {
+    document.getElementById("blueColor").style.color = "blue";
+}
